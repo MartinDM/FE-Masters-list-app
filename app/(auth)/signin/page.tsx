793e24096fde9854705aeda1d44e334f750c116f
martindm <hello@martindm.uk>
2023-03-22 11:01:38 +0000

    WIP

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,3 +1,5 @@
-export default function Signin() {
-    return <p>Signin</p>
+import AuthForm from "@/components/AuthForm";
+
+export default function Register() {
+    return <AuthForm mode="signin" />;
 }
